c501797dc67096e3aebdf17e460b842dc69a5376
kiraaa99 <akbarhost666@gmail.com>
2023-05-06 21:08:08 +0700

    better

--- bot.py
+++ bot.py
@@ -4,36 +4,38 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pickle
 import time
+from webdriver_manager.chrome import ChromeDriverManager
 
+# Constants
 file_nomor = "nomor.txt"
 CHROMEDRIVER_PATH = "C:\chromeDriver\chromedriver.exe"
 MESSAGE_BOX_XPATH = '//div[@class="_3FRCZ copyable-text selectable-text"][@data-tab="6"]'
 SEND_BUTTON_XPATH = '//button[@class="_3M-N-"]'
 
-# mendapatkan cookie
+# Functions
 def get_cookies(driver):
+    ##Get WhatsApp web cookies
     driver.get('https://web.whatsapp.com/')
     input("Scan QR code kemudian tekan enter ")
     pickle.dump(driver.get_cookies(), open('cookies.pkl', 'wb'))
     return pickle.load(open('cookies.pkl', 'rb'))
 
 def set_cookies(driver, cookies):
+    ##Set WhatsApp web cookies
     for cookie in cookies:
         driver.add_cookie(cookie)
 
-def option():
-    opsi = webdriver.ChromeOptions()
-    return opsi.add_argument('--disable-usb-devices')
-
-def read():
+def read(file_nomor):
+    ##baca nomor dari file
     with open(file_nomor, mode="r") as file:
         return file.readlines()
 
 def send(driver, phone_number, message):
+    ##mengirim pesan dan mencari element message box
     driver.get(f'https://web.whatsapp.com/send?phone={phone_number}')
     time.sleep(5)
     try:
-        wait = WebDriverWait(driver, 30)
+        wait = WebDriverWait(driver, 25)
         wait.until(EC.presence_of_element_located((By.XPATH, MESSAGE_BOX_XPATH)))
         message_box = driver.find_element_by_xpath(MESSAGE_BOX_XPATH)
         message_box.send_keys(message)
@@ -44,14 +46,14 @@ def send(driver, phone_number, message):
         print(f"Error sending message to {phone_number}")
 
 def main():
-    # membaca nomor
-    phone_numbers = read()
-    # pesan dari user
+    ## membaca nomor
+    phone_numbers = read(file_nomor)
+    ## pesan dari user
     message = input("Masukkan pesan: ")
-    # menginisialisasi driver dan membuka whatsapp web
-    driver = webdriver.Chrome(CHROMEDRIVER_PATH, options=option())
+    ## Inisialisasi driver dan set cookies
+    driver = webdriver.Chrome(ChromeDriverManager().install())
     set_cookies(driver, get_cookies(driver))
-    # mengirim pesan ke setiap nomor
+    ## mengirim pesan ke setiap nomor
     for phone_number in phone_numbers:
         send(driver, phone_number, message)
     driver.quit()
