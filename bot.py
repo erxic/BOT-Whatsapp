9e2bf2d5d52ab6f7b9d880c1715866a522c3bb61
kiraaa99 <akbarhost666@gmail.com>
2023-05-07 13:41:01 +0700

    better

--- bot.py
+++ bot.py
@@ -2,28 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import pickle
 import time
 import selenium.webdriver.chrome.service as service
 
 # Constants
 file_nomor = "nomor.txt"
-MESSAGE_BOX_XPATH = '//div[@class="_3FRCZ copyable-text selectable-text"][@data-tab="6"]'
-SEND_BUTTON_XPATH = '//button[@class="_3M-N-"]'
+MESSAGE_BOX_ELEMENT = 'div[title="Search or start new chat"]'
+SEND_BUTTON_ELEMENT = 'div[class="_2xy_p _3XKXx"]'
 
 # Functions
-def get_cookies(driver):
-    ##Get WhatsApp web cookies
-    driver.get('https://web.whatsapp.com/')
-    input("Scan QR code kemudian tekan enter ")
-    pickle.dump(driver.get_cookies(), open('cookies.pkl', 'wb'))
-    return pickle.load(open('cookies.pkl', 'rb'))
-
-def set_cookies(driver, cookies):
-    ##Set WhatsApp web cookies
-    for cookie in cookies:
-        driver.add_cookie(cookie)
-
 def read(file_nomor):
     ##baca nomor dari file
     with open(file_nomor, mode="r") as file:
@@ -31,18 +18,17 @@ def read(file_nomor):
 
 def send(driver, phone_number, message):
     ##mengirim pesan dan mencari element message box
-    
     driver.get(f'https://web.whatsapp.com/send?phone={phone_number}')
     time.sleep(5)
     try:
-        wait = WebDriverWait(driver, 25)
-        wait.until(EC.presence_of_element_located((By.XPATH, MESSAGE_BOX_XPATH)))
-        message_box = driver.find_element_by_xpath(MESSAGE_BOX_XPATH)
+        wait = WebDriverWait(driver, 30)
+        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, MESSAGE_BOX_ELEMENT)))
+        message_box = driver.find_element_by_css_selector(By.CSS_SELECTOR,MESSAGE_BOX_ELEMENT)
         message_box.send_keys(message)
-        send_button = driver.find_element_by_xpath(SEND_BUTTON_XPATH)
+        send_button = driver.find_elemnt_by_css_selector(By.CSS_SELECTOR,SEND_BUTTON_ELEMENT)
         send_button.click()
         time.sleep(5)
-    except Exception:
+    except:
         print(f"Error sending message to {phone_number}")
 
 def main():
@@ -52,8 +38,8 @@ def main():
     message = input("Masukkan pesan: ")
     ## Inisialisasi driver dan set cookies
     serv = service.Service('C:\chromeDriver\chromedriver.exe')
-    driver = webdriver.Chrome(service=serv)
-    set_cookies(driver, get_cookies(driver))
+    driver = webdriver.Chrome(service=serv) 
+    driver.get("https://web.whatsapp.com/")
     ## mengirim pesan ke setiap nomor
     for phone_number in phone_numbers:
         send(driver, phone_number, message)
