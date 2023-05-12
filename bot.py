2d9231824e780ff97970f969a57a4b7b7805108e
kiraaa99 <akbarhost666@gmail.com>
2023-05-12 14:47:25 +0700

    clear

--- bot.py
+++ bot.py
@@ -1,37 +1,82 @@
 import pywhatkit as pwk
 import pyautogui
-import time 
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+import cv2
+import numpy as np
+import pyzbar.pyzbar as pyzbar
+import time
 
-
-def read(file_nomor):
-    with open(file_nomor, mode="r") as file:
+#Fungsi membaca nomor dari file
+def read_numbers(file_name):
+    with open(file_name, mode="r") as file:
         return file.readlines()
+
+#fungsi membaca pesan dari file
+def read_message(file_name):
+    with open(file_name, mode="r",encoding="utf-8") as file:
+        return file.read()
     
-def send(numbers, message):
+def is_whatsapp_logged_in():
+    # Tangkap screenshot layar
+    screenshot = pyautogui.screenshot()
+    screenshot_array = np.array(screenshot)
+
+    # Ubah ke format grayscale
+    gray = cv2.cvtColor(screenshot_array, cv2.COLOR_BGR2GRAY)
+
+    # Deteksi QR code
+    barcodes = pyzbar.decode(gray)
+
+    # Jika ada QR code yang terdeteksi, pengguna belum login
+    if barcodes:
+        return False
+
+    return True
+
+#fungsi untuk mengirim pesan 
+def send_message(numbers, message):
     for number in numbers:
         try:
-            pwk.sendwhatmsg_instantly(number, message, 0, True)
-            time.sleep(3) 
-            window_position = pyautogui.locateOnScreen('apple-touch-icon.png',)
-            input_field_position = pyautogui.locateOnScreen('',)
-            send_button_x = window_position.left + window_position.width - 50
-            send_button_y = input_field_position.top + input_field_position.height + 10
-            pyautogui.click(send_button_x, send_button_y)
+            while not is_whatsapp_logged_in():
+                time.sleep(1)
+            pwk.sendwhatmsg_instantly(number, message, tab_close=True)
+            pyautogui.press("enter")
         except Exception as e:
             print(f"Error: {e}")
-            return None
+            return False
     return True
 
 def main():
-    file_numbers = "nomor.txt"
-    numbers = read(file_numbers)
-    message = input("Masukkan Pesan : ")
-    send_stat = send(numbers, message)
+    ## Memilih file nomor menggunakan GUI
+    Tk().withdraw()
+    numbers_file = askopenfilename(title="Pilih File Nomor", filetypes=[("Text Files", "*.txt")])
+    if not numbers_file:
+        print("File nomor tidak dipilih. Program berhenti.")
+        return
+
+    numbers = read_numbers(numbers_file)
+    
+    ## Memilih file pesan menggunakan GUI
+    Tk().withdraw()
+    message_file = askopenfilename(title="Pilih File Pesan", filetypes=[("Text Files", "*.txt")])
+    if not message_file:
+        print("File pesan tidak dipilih. Program berhenti.")
+        return
+
+    message = read_message(message_file)
+    while not message:
+        print("Pesan tidak boleh kosong!")
+        message = read_message(message_file)
+        # Tunggu hingga pengguna login
+    
+    send_stat = send_message(numbers, message)
     if send_stat:
         print("Pesan berhasil terkirim")
     else:
         print("Pesan gagal terkirim")
-    
+    while is_whatsapp_logged_in():
+        time.sleep(1)
 
 if __name__ == '__main__':
     main()
