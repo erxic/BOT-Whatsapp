e511f28d776eaf06fcf4fa85bed17657a9b8d69c
kiraaa99 <akbarhost666@gmail.com>
2023-05-16 11:23:11 +0700

    test

--- bot.py
+++ bot.py
@@ -1,8 +1,7 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from tkinter import Tk
-from tkinter.filedialog import askopenfilename
+import pywhatkit as pwk
+import pyautogui
 import time
+from tkinter import Tk, filedialog, messagebox, Button, Label, Text, PhotoImage, END
 
 # Fungsi membaca nomor dari file
 def read_numbers(file_name):
@@ -14,94 +13,99 @@ def read_message(file_name):
     with open(file_name, mode="r", encoding="utf-8") as file:
         return file.read()
 
-# Fungsi untuk mengirim pesan
-def send_message(numbers, message):
-    driver = webdriver.Chrome()  # Ganti dengan driver yang sesuai, seperti Firefox atau Safari
-    driver.get("https://web.whatsapp.com/")
-    time.sleep(20)  # Tunggu pengguna untuk login ke WhatsApp Web
-
-    for number in numbers:
+# Fungsi untuk menunggu hingga WhatsApp Web siap untuk interaksi
+def wait_for_whatsapp_web():
+    while True:
         try:
-            # Buka chat dengan nomor telepon
-            chat_input = driver.find_element_by_xpath('//div[@contenteditable="true"][@data-tab="3"]')
-            chat_input.send_keys(number)
-            time.sleep(2)
-            chat_input.send_keys(Keys.ENTER)
-            time.sleep(2)
-
-            # Ketik pesan
-            chat_input = driver.find_element_by_xpath('//div[@contenteditable="true"][@data-tab="1"]')
-            chat_input.send_keys(message)
-
-            # Kirim pesan
-            send_button = driver.find_element_by_xpath('//span[@data-icon="send"]')
-            send_button.click()
-
-            if not wait_for_message_sent(driver):
-                print(f"Error: Pesan ke nomor {number} gagal terkirim")
-                time.sleep(1)
-                print(f"Mengirim ulang pesan ke nomor {number}")
-
-                # Ketik pesan lagi
-                chat_input = driver.find_element_by_xpath('//div[@contenteditable="true"][@data-tab="1"]')
-                chat_input.send_keys(message)
-
-                # Kirim pesan lagi
-                send_button = driver.find_element_by_xpath('//span[@data-icon="send"]')
-                send_button.click()
+            pwk.search("https://web.whatsapp.com/send?phone=")
+            time.sleep(1)
+            break
         except Exception as e:
             print(f"Error: {e}")
-            driver.quit()
-            return False
+            continue
 
-    driver.quit()
+# Fungsi untuk mengirim pesan
+def send_message(numbers, message):
+    wait_for_whatsapp_web()
+    time.sleep(5) # Menunggu tambahan waktu setelah WhatsApp Web siap
+    for number in numbers:
+        try:
+            pwk.sendwhatmsg_instantly(number, message, wait_time=15, tab_close=True)
+            pyautogui.press("enter")  # Menunggu waktu yang ditentukan oleh pengguna
+        except Exception as e:
+            print(f"Error: {e}")
+            return False
     return True
 
-# Fungsi untuk menunggu pesan terkirim
-def wait_for_message_sent(driver):
-    timeout = 30  # Waktu maksimal menunggu dalam detik
-    start_time = time.time()
-    while time.time() - start_time < timeout:
-        # Implementasikan logika pengecekan pesan terkirim di sini
-        # Misalnya, Anda dapat memeriksa apakah pesan sudah muncul dalam riwayat chat atau tampilan notifikasi pesan terkirim
-        # Jika pesan terkirim, kembalikan True
-        # Jika pesan belum terkirim, lanjutkan menunggu
-        pass
-    return False
+# Fungsi untuk mengirim pesan dan menampilkan notifikasi
+def send_and_notify():
+    numbers_file = numbers_text.get("1.0", END).strip()
+    message_file = message_text.get("1.0", END).strip()
 
-def main():
-    ## Memilih file nomor menggunakan GUI
-    Tk().withdraw()
-    numbers_file = askopenfilename(title="Pilih File Nomor", filetypes=[("Text Files", "*.txt")])
     if not numbers_file:
-        print("File nomor tidak dipilih. Program berhenti.")
-        return False
+        print("Error: File nomor tidak dipilih.")
+        return
+    if not message_file:
+        print("Error: File pesan tidak dipilih.")
+        return
 
     numbers = read_numbers(numbers_file)
-
-    ## Memilih file pesan menggunakan GUI
-    Tk().withdraw()
-    message_file = askopenfilename(title="Pilih File Pesan", filetypes=[("Text Files", "*.txt")])
-    if not message_file:
-
-        print("File pesan tidak dipilih. Program berhenti.")
-        return False
-
     message = read_message(message_file)
     while not message:
-        print("Pesan tidak boleh kosong!")
-        message = read_message(message_file)
-        Tk().withdraw()
-        message_file = askopenfilename(title="Pilih File Pesan", filetypes=[("Text Files", "*.txt")])
-        if not message_file:
-            print("File pesan tidak dipilih. Program berhenti.")
-            return False
+        print("Error: Pesan tidak boleh kosong.")
+        return
 
-    send_stat = send_message(numbers, message)
-    if send_stat:
-        print("Pesan berhasil terkirim")
+    # Tampilkan konfirmasi menggunakan MessageBox
+    confirm = messagebox.askquestion("Konfirmasi", "Anda yakin ingin mengirim pesan?")
+    if confirm == "yes":
+        send_stat = send_message(numbers, message)
+        if send_stat:
+            messagebox.showinfo("Informasi", "Pesan berhasil terkirim.")
+        else:
+            messagebox.showerror("Error", "Pesan gagal terkirim.")
     else:
-        print("Pesan gagal terkirim")
+        messagebox.showinfo("Informasi", "Pesan dibatalkan.")
 
-if __name__ == '__main__':
-    main()
+# Fungsi untuk memilih file nomor
+def choose_numbers_file():
+    file_path = filedialog.askopenfilename(title="Pilih File Nomor", filetypes=[("Text Files", "*.txt")])
+    numbers_text.delete("1.0", END)
+    numbers_text.insert(END, file_path)
+
+# Fungsi untuk memilih file pesan
+def choose_message_file():
+    file_path = filedialog.askopenfilename(title="Pilih File Pesan", filetypes=[("Text Files", "*.txt")])
+    message_text.delete("1.0", END)
+    message_text.insert(END, file_path)
+
+# Membuat GUI
+window = Tk()
+window.title("Aplikasi Pengiriman Pesan WhatsApp")
+window.geometry("650x300")
+bg_image = PhotoImage(file="E:\BOT\img\Black-Gradiant.png")
+
+bg_label = Label(window, image=bg_image)
+bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+numbers_label = Label(window, text="File Nomor:", font= ('Helvetica bold', 10),)
+numbers_label.pack()
+
+numbers_text = Text(window, height=2.5, cursor="pencil",)
+numbers_text.pack()
+
+numbers_button = Button(window, text="Pilih", cursor="hand1", command=choose_numbers_file)
+numbers_button.pack()
+
+message_label = Label(window, text="File Pesan:", font= ('Helvetica bold', 10))
+message_label.pack()
+
+message_text = Text(window, height=2.5, cursor="pencil")
+message_text.pack()
+
+message_button = Button(window, text="Pilih", command=choose_message_file, cursor="hand1")
+message_button.pack()
+
+send_button = Button(window, activeforeground="green", text="Kirim Pesan", command=send_and_notify, cursor="hand1")
+send_button.pack()
+
+window.mainloop()
